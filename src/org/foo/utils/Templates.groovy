e270bda21d956214655ddc54a9eb14521447429c
glitch-ux <71816777+glitch-ux@users.noreply.github.com>
2022-11-02 23:29:30 +0000

    Update Templates.groovy

--- src/org/foo/utils/Templates.groovy
+++ src/org/foo/utils/Templates.groovy
@@ -2,14 +2,14 @@ package org.foo.utils
 
 public void netTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'net', image: 'agent-windows:latest')]) {
+        containers: [containerTemplate(name: 'net', image: 'agent-windows:latest', command: 'sleep', args: '99d')]) {
     body.call()
 }
 }
 
 public void mavenTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'maven', image: 'agent:2')]) {
+        containers: [containerTemplate(name: 'maven', image: 'agent:2', command: 'sleep', args: '99d')]) {
     body.call()
 }
 }
