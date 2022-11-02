65916053e5c3ffa9204b61bc6a29ca597bdf3393
glitch-ux <71816777+glitch-ux@users.noreply.github.com>
2022-11-02 23:27:15 +0000

    Update Templates.groovy

--- src/org/foo/utils/Templates.groovy
+++ src/org/foo/utils/Templates.groovy
@@ -2,16 +2,14 @@ package org.foo.utils
 
 public void netTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'net', image: 'agent-windows:latest')],
-        volumes: [hostPathVolume(hostPath: '//var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+        containers: [containerTemplate(name: 'net', image: 'agent-windows:latest')]) {
     body.call()
 }
 }
 
 public void mavenTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'maven', image: 'agent:2')],
-         volumes: [hostPathVolume(hostPath: '//var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+        containers: [containerTemplate(name: 'maven', image: 'agent:2')]) {
     body.call()
 }
 }
