0899f493145546c99a7a583537f2ba28ccf69e57
jmaia <jose.p.maia@noesis.pt>
2022-11-02 21:46:45 +0000

    first

---
+++ src/org/foo/utils/Templates.groovy
@@ -0,0 +1,19 @@
+package src.org.utils
+
+public void netTemplate(body) {
+  podTemplate(
+        containers: [containerTemplate(name: 'net', image: 'agent-windows:latest')],
+        volumes: [hostPathVolume(hostPath: '//var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+    body.call()
+}
+}
+
+public void mavenTemplate(body) {
+  podTemplate(
+        containers: [containerTemplate(name: 'maven', image: 'agent:2')],
+         volumes: [hostPathVolume(hostPath: '//var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+    body.call()
+}
+}
+
+return this
